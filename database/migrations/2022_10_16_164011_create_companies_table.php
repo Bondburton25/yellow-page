a5e1b1e4abd5fdd05e9204307bb4a5630806eb51
Bond <kraiwit.dev@gmail.com>
2022-10-16 23:42:51 +0700

    updated companies migration

--- database/migrations/2022_10_16_164011_create_companies_table.php
+++ database/migrations/2022_10_16_164011_create_companies_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('website');
+            $table->string('address');
+            $table->string('phone');
             $table->timestamps();
         });
     }
